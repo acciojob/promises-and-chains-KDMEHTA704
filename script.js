71f7391cde994bc796f58e5608bc1958d9111089
KDMEHTA704 <131295594+KDMEHTA704@users.noreply.github.com>
2025-03-05 16:26:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+    let form = document.getElementById("votingForm");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        let name = document.getElementById("name").value.trim();
+        let age = document.getElementById("age").value.trim();
+        if (name === "" || age === "") {
+            alert("Please enter valid details.");
+            return;
+        }
+
+        age = Number(age);
+
+        let votingPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (age > 18) {
+                    resolve(`Welcome, ${name}. You can vote.`);
+                } else {
+                    reject(`Oh sorry ${name}. You aren't old enough.`);
+                }
+            }, 4000); // 4-second delay
+        });
+		
+        votingPromise
+            .then((message) => alert(message))
+            .catch((errorMessage) => alert(errorMessage));
+    });
